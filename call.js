dbbdd483d6e5dd8ffcbcbd0efa5e0d3686decda6
americanman <snowboardracer7@gmail.com>
2018-06-11 15:41:53 -0400

    Pass arguments to the call method

--- call.js
+++ call.js
@@ -11,3 +11,22 @@ const pride = {
 //call methot 'this' from pride and inject into mockingbird describe method instead of original 'this'(title)
 mockingbird.describe.call(pride);
 //Output will be: Pride and Prejudice is a classic novel
+
+//Here is another example of a call(apply) function with arguments.
+
+const cat = {
+  name: 'Bailey'
+};
+
+function sayHello(message) {
+  console.log(`${message}, ${this.name}`);
+}
+//first argument sets up 'this', and socond one is passing argument to the sayHello function
+sayHello.call(cat, 'Nice to see you');
+//returns Nice to see you Bailey
+
+//apply method does the same, it sets up first argument as 'this', but second argument has to be an array
+sayHello.apply(cat, ['Hello']);
+//Hello, Bailey
+
+
